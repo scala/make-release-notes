140b880eff2591274d46c8633d6dcb540319e25c
Amir Shaikhha <amir.shaikhha@typesafe.com>
2013-06-06 17:04:07 +0200

    Markdown added as target language.
    * New scala-lang.org site uses jekyll. So it's needed to create markdown release notes.
    * You still have the option of generating html file by default unless you explicitly mention that you would need markdown file.

--- src/main/scala/GitInfo.scala
+++ src/main/scala/GitInfo.scala
@@ -3,17 +3,16 @@ case class Commit(sha: String, author: String, header: String, body: String) {
   override def toString = " * " + sha + " (" + author + ") " + header + " - " + body.take(5) + " ..."
 }
 
-
 /** Gobal functions for dealing with git. */
 object GitHelper {
   def processGitCommits(gitDir: java.io.File, previousTag: String, currentTag: String): IndexedSeq[Commit] = {
     import sys.process._
     val gitFormat = "%h %s" // sha and subject
-    val log = Process(Seq("git", "--no-pager", "log", s"${previousTag}..${currentTag}","--format=format:"+gitFormat,"--no-merges", "--topo-order"), gitDir).lines
+    val log = Process(Seq("git", "--no-pager", "log", s"${previousTag}..${currentTag}", "--format=format:" + gitFormat, "--no-merges", "--topo-order"), gitDir).lines
 
     log.par.map(_.split(" ", 2)).collect {
       case Array(sha, title) =>
-        val (author :: body) = Process(Seq("git", "--no-pager", "show", sha ,"--format=format:%aN%n%b","--quiet"), gitDir).lines.toList
+        val (author :: body) = Process(Seq("git", "--no-pager", "show", sha, "--format=format:%aN%n%b", "--quiet"), gitDir).lines.toList
         Commit(sha, author, title, body.mkString("\n"))
     }.toVector
   }
@@ -21,54 +20,52 @@ object GitHelper {
   def hasFixins(msg: String): Boolean = (
     (msg contains "SI-") /*&& ((msg.toLowerCase contains "fix") || (msg.toLowerCase contains "close"))*/
   )
-  
+
   val siPattern = java.util.regex.Pattern.compile("(SI-[0-9]+)")
 
-  def fixLinks(commit: Commit): String = {
+  def fixLinks(commit: Commit)(implicit targetLanguage: TargetLanguage): String = {
     val searchString = commit.body + commit.header
     val m = siPattern matcher searchString
     val issues = new collection.mutable.ArrayBuffer[String]
-    while(m.find()) {
+    while (m.find()) {
       issues += (m group 1)
     }
-    issues map (si => """<a href="https://issues.scala-lang.org/browse/%s">%s</a>""" format (si, si)) mkString ", "
+    issues map (si => targetLanguage.createHyperLink(s"https://issues.scala-lang.org/browse/$si", si)) mkString ", "
   }
 
   def htmlEncode(s: String) = org.apache.commons.lang3.StringEscapeUtils.escapeHtml4(s)
 }
 
-class GitInfo(gitDir: java.io.File, val previousTag: String, val currentTag: String) {
+class GitInfo(gitDir: java.io.File, val previousTag: String, val currentTag: String)(implicit targetLanguage: TargetLanguage) {
   import GitHelper._
   val commits = processGitCommits(gitDir, previousTag, currentTag)
 
   val authors: Seq[(String, Int)] = {
-    val grouped: Vector[(String,Int)] = (commits groupBy (_.author)).map { case (a,c) => a -> c.length }{collection.breakOut}
+    val grouped: Vector[(String, Int)] = (commits groupBy (_.author)).map { case (a, c) => a -> c.length } { collection.breakOut }
     (grouped sortBy (_._2)).reverse
   }
 
   val fixCommits =
-   for {
-     commit <- commits
-     searchString = commit.body + commit.header
-     if hasFixins(searchString)
-   } yield commit
+    for {
+      commit <- commits
+      searchString = commit.body + commit.header
+      if hasFixins(searchString)
+    } yield commit
 
-  private def commitShaLink(sha: String) = 
-    s"""<a href="https://github.com/scala/scala/commit/${sha}">${sha}</a>"""
+  private def commitShaLink(sha: String) =
+    targetLanguage.createHyperLink(s"https://github.com/scala/scala/commit/${sha}", sha)
 
-  private def blankLine(): String = "<p>&nbsp;</p>"
-  private def header4(msg: String): String = s"<h4>$msg</h4>"
+  private def blankLine(): String = targetLanguage.blankLine()
+  private def header4(msg: String): String = targetLanguage.header4(msg)
 
   def renderCommitterList: String = {
     val sb = new StringBuffer
     sb append blankLine()
     sb append header4("A big thank you to all the contributors!")
-    sb append """|<table border="0" cellspacing="0" cellpadding="1">
-                 |  <thead><tr><th>#</th><th align="left">Author</th></tr></thead>
-                 |<tbody>""".stripMargin
-    for((author, count) <- authors)
-      sb append s"""<tr><td align="right">${count} &nbsp;</td><td>${htmlEncode(author)}</td></tr>"""
-    sb append """</tbody></table>"""
+    sb append targetLanguage.tableHeader("#", "Author")
+    for ((author, count) <- authors)
+      sb append targetLanguage.tableRow(count.toString, author)
+    sb append targetLanguage.tableEnd
     sb.toString
   }
 
@@ -76,13 +73,10 @@ class GitInfo(gitDir: java.io.File, val previousTag: String, val currentTag: Str
     val sb = new StringBuffer
     sb append blankLine()
     sb append header4("Complete commit list!")
-    sb append """<table border="0" cellspacing="0" cellpadding="1">
-      <thead><tr><th>sha</th><th align="left">Title</th></tr></thead>
-    <tbody>"""
-    for(commit <- commits)
-       sb append s"""<tr><td align="right">${commitShaLink(commit.sha)}&nbsp;</td><td>${htmlEncode(commit.header)}</td></tr>"""
-    sb append """</tbody>
-      </table>"""
+    sb append targetLanguage.tableHeader("sha", "Title")
+    for (commit <- commits)
+      sb append targetLanguage.tableRow(commitShaLink(commit.sha), commit.header)
+    sb append targetLanguage.tableEnd
     sb.toString
   }
 
@@ -90,16 +84,12 @@ class GitInfo(gitDir: java.io.File, val previousTag: String, val currentTag: Str
     val sb = new StringBuffer
     sb append blankLine()
     sb append header4(s"Commits and the issues they fixed since ${previousTag}")
-    sb append ("""<table border="0" cellspacing="0" cellpading="1">
-      <thead><tr><th>Issue(s)</th><th>Commit</th><th>Message</th></tr></thead>
-    <tbody>""")
-    for(commit <- fixCommits)
-      sb append s"""<tr><td>${fixLinks(commit)}&nbsp;</td><td>${commitShaLink(commit.sha)}&nbsp;</td><td>${htmlEncode(commit.header)}</td></tr>"""
-    sb append """</tbody>
-      </table>"""
+    sb append targetLanguage.tableHeader("Issue(s)", "Commit", "Message")
+    for (commit <- fixCommits)
+      sb append targetLanguage.tableRow(fixLinks(commit), commitShaLink(commit.sha), commit.header)
+    sb append targetLanguage.tableEnd
     sb append blankLine()
     sb.toString
   }
 
-   
 }
---
+++ src/main/scala/TargetLanguage.scala
@@ -0,0 +1,56 @@
+sealed trait TargetLanguage {
+  def createHyperLink(link: String, content: String): String
+  def blankLine(): String
+  def header4(msg: String): String
+  def tableHeader(firstColumn: String, secondColumn: String): String
+  def tableRow(firstColumn: String, secondColumn: String): String
+  def tableHeader(firstColumn: String, secondColumn: String, thirdColumn: String): String
+  def tableRow(firstColumn: String, secondColumn: String, thirdColumn: String): String
+  def tableEnd: String
+}
+case object MarkDown extends TargetLanguage {
+  def createHyperLink(link: String, content: String): String =
+    s"[$link]($content)"
+  def blankLine(): String = "\n"
+  def header4(msg: String): String = s"#### $msg\n"
+  def tableHeader(firstColumn: String, secondColumn: String): String =
+    s"""
+${markdownEncode(firstColumn)} | $secondColumn
+---: | ---
+"""
+  def tableRow(firstColumn: String, secondColumn: String): String = s"${firstColumn} | <notextile>${escapeHtml(secondColumn)}</notextile>\n"
+  def tableHeader(firstColumn: String, secondColumn: String, thirdColumn: String): String =
+    s"""
+$firstColumn | $secondColumn | $thirdColumn
+--- | --- | ---
+"""
+  def tableRow(firstColumn: String, secondColumn: String, thirdColumn: String): String = s"$firstColumn | $secondColumn | <notextile>${escapeHtml(thirdColumn)}</notextile>\n"
+  def tableEnd: String = "\n"
+
+  def markdownEncode(s: String): String = s.flatMap {
+    case c if (List('*', '`', '[', ']', '#').contains(c)) => "\\" + c
+    case x => x.toString
+  }
+
+  def escapeHtml(s: String): String = Html.htmlEncode(s).flatMap {
+    case '|' => "&#124;" // it would destroy tables!
+    case c => c.toString
+  }
+}
+case object Html extends TargetLanguage {
+  def createHyperLink(link: String, content: String): String =
+    s"""<a href="$link">$content</a>"""
+  def blankLine(): String = "<p>&nbsp;</p>"
+  def header4(msg: String): String = s"<h4>$msg</h4>"
+  def tableHeader(firstColumn: String, secondColumn: String): String = s"""|<table border="0" cellspacing="0" cellpadding="1">
+                 |  <thead><tr><th>$firstColumn</th><th align="left">$secondColumn</th></tr></thead>
+                 |<tbody>""".stripMargin
+  def tableRow(firstColumn: String, secondColumn: String): String = s"""<tr><td align="right">${firstColumn} &nbsp;</td><td>${htmlEncode(secondColumn)}</td></tr>"""
+  def tableHeader(firstColumn: String, secondColumn: String, thirdColumn: String): String = s"""<table border="0" cellspacing="0" cellpading="1">
+      <thead><tr><th>$firstColumn</th><th>$secondColumn</th><th>$thirdColumn</th></tr></thead>
+    <tbody>"""
+  def tableRow(firstColumn: String, secondColumn: String, thirdColumn: String): String = s"""<tr><td>${firstColumn}&nbsp;</td><td>${secondColumn}&nbsp;</td><td>${htmlEncode(thirdColumn)}</td></tr>"""
+  def tableEnd: String = "</tbody></table>"
+
+  def htmlEncode(s: String) = org.apache.commons.lang3.StringEscapeUtils.escapeHtml4(s)
+}
