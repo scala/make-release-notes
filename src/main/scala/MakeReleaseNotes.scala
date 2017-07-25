5d7f8de61447660821f891c5d1bdee8875f39812
Jason Zaugg <jzaugg@gmail.com>
2017-07-25 11:20:56 +1000

    Adapting to https://github.com/scala/scala-lang/pull/679

--- src/main/scala/MakeReleaseNotes.scala
+++ src/main/scala/MakeReleaseNotes.scala
@@ -109,7 +109,7 @@ object MakeReleaseNotes {
       </body>
     </html>"""
       case MarkDown => s"""---
-layout: news
+layout: inner-page-no-masthead
 post-type: announcement
 permalink: /news/$version
 title: "Scala ${currentTag drop 1} is now available!"
